fb83da0dad37b352db8fdd368f06b12bb04b07c7
Y99dr451l <matthias@kockisch.eu>
2024-06-03 14:46:30 +0200

    moved Diablo to omni robot class

---
+++ diablo.py
@@ -0,0 +1,147 @@
+from typing import Optional
+import numpy as np
+import torch
+from omni.isaac.core.robots.robot import Robot
+from omni.isaac.core.utils.prims import get_prim_at_path
+from omni.isaac.core.utils.stage import add_reference_to_stage
+from pxr import UsdPhysics
+from pxr import PhysxSchema
+from typing import Optional, Union
+
+class Diablo(Robot):
+
+    def __init__(
+        self,
+        prim_path: str,
+        name: Optional[str] = "diablo",
+        usd_path: Optional[str] = "./diablo.usd",
+        translation: Optional[Union[list, np.array, torch.tensor]] = torch.tensor([1.0, 0.0, 0.0]),
+        orientation: Optional[Union[list, np.array, torch.tensor]] = torch.tensor([0.0, 0.0, 0.0, 1.0]),
+    ) -> None:
+        '''Initializes the Diablo robot.
+
+        Args:
+            - prim_path: path to the robot in the stage
+            - name: name of the robot
+            - usd_path: path to the USD file of the robot
+            - translation: initial translation of the robot
+            - orientation: initial orientation of the robot
+        '''
+        self._usd_path = usd_path
+        self._name = name
+        self._position = torch.tensor(translation)
+        self._orientation = torch.tensor(orientation)
+
+        add_reference_to_stage(self._usd_path, prim_path)
+
+        super().__init__(
+            prim_path=prim_path,
+            name=name,
+            translation=self._position,
+            orientation=self._orientation,
+            articulation_controller=None #
+        )
+
+        self.indices = { # very well ordered joint indices
+            "left_base": 1, # hip
+            "right_base": 0,
+            "left_leg": 2, # knee
+            "right_leg": 5,
+            "left_knee": 3, # placeholder for knee motor on real robot, located at the hip
+            "right_knee": 6, # here they do the same as _base
+            "left_wheel": 4, # wheels
+            "right_wheel": 7
+        }
+
+        dof_paths = [
+            "/base_link/Rev1",
+            "/base_link/Rev2",
+            "/motor_left_link_1/Rev3",
+            "/leg_left_link_1/Rev4", 
+            "/leg2_left_link_1/Rev5",
+            "/motor_right_link_1/Rev6",
+            "/leg_right_link_1/Rev7",
+            "/leg2_right_link_1/Rev8"
+        ]
+
+        # to change, values are in SI units
+        drive_type = ["angular"] * 8
+        self.target_type = ["velocity"] * 8
+        default_dof_pos = [0.] * 8
+        stiffness = [15000] * 8
+        damping = [1] * 8
+        max_force = [1000] * 8
+        max_velocity = [1000] * 8
+
+        self.joints = []
+        for i, dof in enumerate(dof_paths):
+            joint_prim = get_prim_at_path(f"{self.prim_path}{dof}")
+            drive = UsdPhysics.DriveAPI.Apply(joint_prim, drive_type[i])
+
+            if self.target_type == "position":
+                if not drive.GetTargetPositionAttr(): drive.CreateTargetPositionAttr(default_dof_pos[i])
+                else: drive.GetTargetPositionAttr().Set(default_dof_pos[i])
+            elif self.target_type == "velocity":
+                if not drive.GetTargetVelocityAttr(): drive.CreateTargetVelocityAttr(default_dof_pos[i])
+                else: drive.GetTargetVelocityAttr().Set(default_dof_pos[i])
+            if not drive.GetStiffnessAttr(): drive.CreateStiffnessAttr(stiffness[i])
+            else: drive.GetStiffnessAttr().Set(stiffness[i])
+            if not drive.GetDampingAttr(): drive.CreateDampingAttr(damping[i])
+            else: drive.GetDampingAttr().Set(damping[i])
+            if not drive.GetMaxForceAttr(): drive.CreateMaxForceAttr(max_force[i])
+            else: drive.GetMaxForceAttr().Set(max_force[i])
+            PhysxSchema.PhysxJointAPI(get_prim_at_path(f"{self.prim_path}{dof}")).CreateMaxJointVelocityAttr().Set(max_velocity[i])
+
+            self.joints.append(drive)
+    
+    def set_joint_target(self, index: Union[int, str], target: float):
+        '''Sets the velocity target for a joint in the robot.
+
+        Indices are either ints or the following strings (in order of indices):
+            right_base, left_base, left_leg, left_knee, left_wheel, right_leg, right_knee, right_wheel
+        Args:
+            - index: index of the joint to set the velocity target
+            - target: target velocity for the joint
+        '''
+        if isinstance(index, str):
+            index = self.indices[index]
+        if self.target_type[index] == "position":
+            self.joints[index].GetTargetPositionAttr().Set(target)
+        elif self.target_type[index] == "velocity":
+            self.joints[index].GetTargetVelocityAttr().Set(target)
+
+    def set_joint_targets(self, target: Optional[Union[list, np.array]]) -> None:
+        '''Sets the velocity target for all joints in the robot.
+
+        Joint order is: right_base, left_base, left_leg, left_knee, left_wheel, right_leg, right_knee, right_wheel
+        Args:
+            - target: list of target velocities for each joint in the robot
+        '''
+        for i in len(self.joints):
+            self.set_joint_target(i, target[i])
+
+    def set_joint_position(self, index: Union[int, str], position):
+        '''Sets the position target for a joint in the robot.
+
+        Indices are either ints or the following strings (in order of indices):
+            right_base, left_base, left_leg, left_knee, left_wheel, right_leg, right_knee, right_wheel
+        Args:
+            - index: index of the joint to set the position target
+            - position: target position for the joint
+        '''
+        if isinstance(index, str):
+            index = self.indices[index]
+        self.joints[index].GetTargetPositionAttr().Set(position)
+
+    def set_joint_positions(self, position: Optional[Union[list, np.array]]) -> None:
+        '''Sets the position target for all joints in the robot.
+
+        Joint order is: right_base, left_base, left_leg, left_knee, left_wheel, right_leg, right_knee, right_wheel
+        Args:
+            - position: list of target positions for each joint in the robot
+        '''
+        for i in len(self.joints):
+            self.set_joint_position(i, position[i])
+
+    def set_world_pose(self, position: torch.tensor, orientation: torch.tensor) -> None: # the usd origin is weird
+        super().set_world_pose(position + np.array([-0.05, 0, 0.05]), orientation + np.array([0.5, 0, -0.8660254, 0]))
--- run_diablo.py
+++ run_diablo.py
@@ -18,7 +18,7 @@ dark_count_rate = 1.
 ### SIM SETUP
 @hydra.main(config_name="config", config_path="cfg")
 def run(cfg: DictConfig):
-    # config
+    # hydra config for OmniLRS
     global simulation_app, world, timeline, SM, use_omnilrs, use_ros2
     cfg = omegaconfToDict(cfg)
     cfg = instantiateConfigs(cfg)
@@ -26,15 +26,16 @@ def run(cfg: DictConfig):
     use_ros2 = cfg["mode"]["name"] == "ROS2"
     appcfg = cfg["rendering"]["renderer"].__dict__
     appcfg["headless"] = headless
+
     # simulation app
     from omni.isaac.kit import SimulationApp # don't include anything else from omni before creating the SimulationApp
     simulation_app = SimulationApp(appcfg)
-    if use_omnilrs:
+    if use_omnilrs: # moon environment
         from src.environments_wrappers import startSim
         SM, simulation_app = startSim(cfg, simulation_app=simulation_app, dt=dt) # also handles ROS2
         world = SM.world
         timeline = SM.timeline
-    else:
+    else: # default environment
         if use_ros2:
             from src.environments_wrappers.ros2 import enable_ros2
             enable_ros2(simulation_app, bridge_name=cfg["mode"]["bridge_name"])
@@ -46,168 +47,147 @@ def run(cfg: DictConfig):
         world.scene.add_default_ground_plane()
         timeline = omni.timeline.get_timeline_interface()
 
-run() # just for the hydra wrapper
+if __name__ == "__main__":
+    run() # just for the hydra wrapper
 
-### DIABLO UTILS
-# _BASE and _LEG do the same thing (the diablo robot only has 6 motors)
-RIGHT_BASE = 0 # Rev1
-LEFT_BASE = 1 # ...
-LEFT_LEG = 2
-LEFT_KNEE = 3
-LEFT_WHEEL = 4
-RIGHT_LEG = 5
-RIGHT_KNEE = 6
-RIGHT_WHEEL = 7 # Rev8
-
-joint_paths = ["/base_link/Rev1", "/base_link/Rev2", "/motor_left_link_1/Rev3", "/leg_left_link_1/Rev4", 
-               "/leg2_left_link_1/Rev5", "/motor_right_link_1/Rev6", "/leg_right_link_1/Rev7", "/leg2_right_link_1/Rev8"]
-
-def get_diablo_joints(diablo_stage_path):
-    from pxr import UsdPhysics
-    import omni.isaac.core.utils.stage as stage_utils
-    stage = stage_utils.get_current_stage()
-    joints = []
-    for path in joint_paths:
-        joints.append(UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath(diablo_stage_path + path), "angular"))
-        joints[-1].GetDampingAttr().Set(15000)
-        joints[-1].GetStiffnessAttr().Set(0)
-    return joints
-
-diablo_stage_path = "/diablo_simulation"
-from omni.isaac.core.utils.stage import add_reference_to_stage
-add_reference_to_stage("./diablo.usd", diablo_stage_path)
-from omni.isaac.core.robots import Robot
-diablo = Robot(prim_path=diablo_stage_path, name="diablo")
-diablo.set_world_pose(
-    position=np.array(diablo_position) + np.array([-0.05, 0, 0.05]), # second terms correct for diablo's weird origin
-    orientation=np.array([1, 0, 0, 0]) + np.array([0.5, 0, -0.8660254, 0]) # (w, x, y, z) quaternion
-)
-from omni.isaac.core.utils.viewports import set_camera_view
-set_camera_view(eye=np.array(camera_position), target=np.array(diablo_position)) # sets viewport
-joints = get_diablo_joints(diablo_stage_path)
-
-### DIABLO CAMERA
-from omni.isaac.sensor import Camera
-diablo_camera_path = diablo_stage_path + "/base_link/camera"
-diablo_camera = Camera(
-    prim_path=diablo_camera_path,
-    translation=(0.21, 0, 0.67), # integration prototype (approx.)
-    orientation=(0.966, 0, 0.259, 0), # quaternion, 30° down
-    resolution=(1280, 720)
-)
-diablo_camera.set_clipping_range(0.001, 100)
-diablo_camera.set_focal_length(.5)
-
-### CAMERA RENDERING
-import omni.replicator.core as rep
-rp = rep.create.render_product(camera=diablo_camera_path, resolution=(1280, 720), name="rp")
-
-from spad import SPADWriter
-rep.WriterRegistry.register(SPADWriter)
-spad_writer = rep.WriterRegistry.get("SPADWriter")
-spad_writer.initialize(
-    output_path=os.path.join(os.getcwd(), "images"),
-    dt=dt,
-    quantum_efficiency=quantum_efficiency,
-    dark_count_rate=dark_count_rate,
-    num_average=num_average
-)
-spad_writer.attach(rp)
-
-rgb_writer = rep.WriterRegistry.get("BasicWriter")
-rgb_writer.initialize(
-    output_dir=os.path.join(os.getcwd(), "images"),
-    rgb=True
-)
-rgb_writer.attach(rp)
-
-### DIABLO IMU
-from omni.isaac.sensor import IMUSensor
-diablo_imu = IMUSensor(
-    prim_path=diablo_stage_path + "/base_link/imu_sensor",
-    translation=np.array([0, 0, 0]), # to be changed
-)
-
-### ROS2 CAMERA STREAM
-if use_ros2 and False: # not working yet
-    import omni.graph.core as og
-    import usdrt.Sdf
-    keys = og.Controller.Keys
-    (ros_camera_graph, _, _, _) = og.Controller.edit(
-        {
-            "graph_path": "/ROS_Camera",
-            "evaluator_name": "push",
-            "pipeline_stage": og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_ONDEMAND,
-        },
-        {
-            keys.CREATE_NODES: [
-                ("OnTick", "omni.graph.action.OnTick"),
-                ("createViewport", "omni.isaac.core_nodes.IsaacCreateViewport"),
-                ("getRenderProduct", "omni.isaac.core_nodes.IsaacGetViewportRenderProduct"),
-                ("setCamera", "omni.isaac.core_nodes.IsaacSetCameraOnRenderProduct"),
-                ("cameraHelperRgb", "omni.isaac.ros2_bridge.ROS2CameraHelper"),
-            ],
-            keys.CONNECT: [
-                ("OnTick.outputs:tick", "createViewport.inputs:execIn"),
-                ("createViewport.outputs:execOut", "getRenderProduct.inputs:execIn"),
-                ("createViewport.outputs:viewport", "getRenderProduct.inputs:viewport"),
-                ("getRenderProduct.outputs:execOut", "setCamera.inputs:execIn"),
-                ("getRenderProduct.outputs:renderProductPath", "setCamera.inputs:renderProductPath"),
-                ("setCamera.outputs:execOut", "cameraHelperRgb.inputs:execIn"),
-                ("getRenderProduct.outputs:renderProductPath", "cameraHelperRgb.inputs:renderProductPath"),
-            ],
-            keys.SET_VALUES: [
-                ("createViewport.inputs:viewportId", 0),
-                ("cameraHelperRgb.inputs:frameId", "sim_camera"),
-                ("cameraHelperRgb.inputs:topicName", "rgb"),
-                ("cameraHelperRgb.inputs:type", "rgb"),
-                ("setCamera.inputs:cameraPrim", [usdrt.Sdf.Path(diablo_camera_path)]),
-            ],
-        },
+    ### DIABLO
+    from diablo import Diablo
+    diablo_stage_path = "/diablo_simulation"
+    diablo = Diablo(
+        prim_path=diablo_stage_path,
+        name="diablo",
+        usd_path="./diablo.usd",
+        translation=diablo_position,
+        orientation=[1, 0, 0, 0] # (w, x, y, z) quaternion
     )
-    og.Controller.evaluate_sync(ros_camera_graph)
-    simulation_app.update()
+    # diablo.set_world_pose(
+    #     position=np.array(diablo_position),
+    #     orientation=np.array([1, 0, 0, 0]) # (w, x, y, z) quaternion
+    # )
 
-### RUN SIMULATION
-left_wheel_joint = joints[LEFT_WHEEL]
-right_wheel_joint = joints[RIGHT_WHEEL]
-world.reset()
-diablo.initialize()
-diablo_camera.initialize()
-diablo_imu.initialize()
-timeline.play()
-world.step(render=False)
+    from omni.isaac.core.utils.viewports import set_camera_view
+    set_camera_view(eye=np.array(camera_position), target=np.array(diablo_position)) # sets viewport
 
-i = 0
-dtime = time.time()
-stime = 0
+    ### DIABLO CAMERA
+    from omni.isaac.sensor import Camera
+    diablo_camera_path = diablo_stage_path + "/base_link/camera" # attached to the main body
+    diablo_camera = Camera(
+        prim_path=diablo_camera_path,
+        translation=(0.21, 0, 0.67), # integration prototype (approx.)
+        orientation=(0.966, 0, 0.259, 0), # quaternion, 30° down
+        resolution=(1280, 720)
+    )
+    diablo_camera.set_clipping_range(0.001, 100)
+    diablo_camera.set_focal_length(.5)
 
-while simulation_app.is_running():
-    world.step(render=True)
-    if world.is_playing():
-        if use_omnilrs and use_ros2: # OmniLRS stuff
-            if world.current_time_step_index == 0:
-                world.reset()
-                SM.ROSLabManager.reset()
-                SM.ROSRobotManager.reset()
-            SM.ROSLabManager.applyModifications()
-            if SM.ROSLabManager.trigger_reset:
-                SM.ROSRobotManager.reset()
-                SM.ROSLabManager.trigger_reset = False
-            SM.ROSRobotManager.applyModifications()
+    ### CAMERA RENDERING
+    import omni.replicator.core as rep
+    rp = rep.create.render_product(camera=diablo_camera_path, resolution=(1280, 720), name="rp")
 
-        ### CONTROL AND SENSING
-        # print(diablo_imu.get_current_frame()) # lin_acc, ang_vel, orientation
-        left_wheel_joint.GetTargetVelocityAttr().Set(i % 200 - 100) # deg/time unit
-        right_wheel_joint.GetTargetVelocityAttr().Set(100 - i % 200)
-        i += 1
+    from spad import SPADWriter
+    rep.WriterRegistry.register(SPADWriter)
+    spad_writer: SPADWriter = rep.WriterRegistry.get("SPADWriter")
+    spad_writer.initialize(
+        output_path=os.path.join(os.getcwd(), "images"),
+        dt=dt,
+        quantum_efficiency=quantum_efficiency,
+        dark_count_rate=dark_count_rate,
+        num_average=num_average
+    )
+    spad_writer.attach(rp)
 
-        frame = spad_writer.get_frame()
+    # rgb_writer = rep.WriterRegistry.get("BasicWriter")
+    # rgb_writer.initialize(
+    #     output_dir=os.path.join(os.getcwd(), "images"),
+    #     rgb=True
+    # )
+    # rgb_writer.attach(rp)
 
-        dtime2 = time.time()
-        print(f"Sim-time: {stime}".ljust(10), f"dt: {dtime2 - dtime}")
-        dtime = dtime2
-        stime = round(stime + dt, 4)
+    ### DIABLO IMU
+    from omni.isaac.sensor import IMUSensor
+    diablo_imu = IMUSensor(
+        prim_path=diablo_stage_path + "/base_link/imu_sensor", # attached to the main body
+        translation=np.array([0, 0, 0]), # to be changed
+    )
 
-timeline.stop()
-simulation_app.close()
+    ### ROS2 CAMERA STREAM
+    if use_ros2 and False: # not working yet
+        import omni.graph.core as og
+        import usdrt.Sdf
+        keys = og.Controller.Keys
+        (ros_camera_graph, _, _, _) = og.Controller.edit(
+            {
+                "graph_path": "/ROS_Camera",
+                "evaluator_name": "push",
+                "pipeline_stage": og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_ONDEMAND,
+            },
+            {
+                keys.CREATE_NODES: [
+                    ("OnTick", "omni.graph.action.OnTick"),
+                    ("createViewport", "omni.isaac.core_nodes.IsaacCreateViewport"),
+                    ("getRenderProduct", "omni.isaac.core_nodes.IsaacGetViewportRenderProduct"),
+                    ("setCamera", "omni.isaac.core_nodes.IsaacSetCameraOnRenderProduct"),
+                    ("cameraHelperRgb", "omni.isaac.ros2_bridge.ROS2CameraHelper"),
+                ],
+                keys.CONNECT: [
+                    ("OnTick.outputs:tick", "createViewport.inputs:execIn"),
+                    ("createViewport.outputs:execOut", "getRenderProduct.inputs:execIn"),
+                    ("createViewport.outputs:viewport", "getRenderProduct.inputs:viewport"),
+                    ("getRenderProduct.outputs:execOut", "setCamera.inputs:execIn"),
+                    ("getRenderProduct.outputs:renderProductPath", "setCamera.inputs:renderProductPath"),
+                    ("setCamera.outputs:execOut", "cameraHelperRgb.inputs:execIn"),
+                    ("getRenderProduct.outputs:renderProductPath", "cameraHelperRgb.inputs:renderProductPath"),
+                ],
+                keys.SET_VALUES: [
+                    ("createViewport.inputs:viewportId", 0),
+                    ("cameraHelperRgb.inputs:frameId", "sim_camera"),
+                    ("cameraHelperRgb.inputs:topicName", "rgb"),
+                    ("cameraHelperRgb.inputs:type", "rgb"),
+                    ("setCamera.inputs:cameraPrim", [usdrt.Sdf.Path(diablo_camera_path)]),
+                ],
+            },
+        )
+        og.Controller.evaluate_sync(ros_camera_graph)
+        simulation_app.update()
+
+    ### RUN SIMULATION
+    world.reset()
+    diablo.initialize()
+    diablo_camera.initialize()
+    diablo_imu.initialize()
+    timeline.play()
+    world.step(render=False)
+
+    i = 0
+    dtime = time.time()
+    stime = 0
+
+    while simulation_app.is_running():
+        world.step(render=True)
+        if world.is_playing():
+            if use_omnilrs and use_ros2: # OmniLRS stuff
+                if world.current_time_step_index == 0:
+                    world.reset()
+                    SM.ROSLabManager.reset()
+                    SM.ROSRobotManager.reset()
+                SM.ROSLabManager.applyModifications()
+                if SM.ROSLabManager.trigger_reset:
+                    SM.ROSRobotManager.reset()
+                    SM.ROSLabManager.trigger_reset = False
+                SM.ROSRobotManager.applyModifications()
+
+            # print(diablo_imu.get_current_frame()) # lin_acc, ang_vel, orientation
+            frame = spad_writer.get_frame() # last SPAD averaged frame
+            subframe = spad_writer.get_subframe() # last SPAD frame
+
+            diablo.set_joint_target("left_wheel", i % 200 - 100)
+            diablo.set_joint_target("right_wheel", 100 - i % 200)
+            i += 1
+
+            dtime2 = time.time()
+            print(f"Sim-time: {str(stime) + " ms":<15}Frame-time: {dtime2 - dtime:.5f} ms")
+            dtime = dtime2
+            stime = round(stime + dt, 4)
+
+    timeline.stop()
+    simulation_app.close()
